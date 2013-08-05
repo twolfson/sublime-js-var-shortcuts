58ebe6bc8389b2b48daa4e690ce391d48647d4e7
Todd Wolfson <todd@twolfson.com>
2013-08-05 01:18:06 -0700

    Taking default action on non-JS files

--- js_var_shortcuts.py
+++ js_var_shortcuts.py
@@ -1,5 +1,14 @@
 import sublime_plugin
 
 class JsVarDeleteCommand(sublime_plugin.TextCommand):
-    def run(self, view):
+    def run(self, edit):
+        # If the view is not JavaScript, perform the default
+        if self.view.settings().get('syntax') != u'Packages/JavaScript/JavaScript.tmLanguage':
+            return self.run_default()
+
         print 'hi'
+
+    def run_default(self):
+        self.view.run_command("run_macro_file", {
+            "file": "Packages/Default/Delete to Hard BOL.sublime-macro"
+        })
