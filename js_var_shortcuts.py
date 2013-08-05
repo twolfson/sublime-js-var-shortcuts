c8bcf6f4e7764fbc214d67d72562db99ffa8c5a0
Todd Wolfson <todd@twolfson.com>
2013-08-05 02:21:10 -0700

    Successfully determining var regions

--- js_var_shortcuts.py
+++ js_var_shortcuts.py
@@ -8,12 +8,29 @@ class JsVarDeleteCommand(sublime_plugin.TextCommand):
         if view.settings().get('syntax') != u'Packages/JavaScript/JavaScript.tmLanguage':
             return self.run_default()
 
-        # Determine which selections are in a `var` block
-        # DEV: This is determined by being between a `var` and a `;`
+        # Grab the `var` and selected regions
+        # DEV: `var` regions are defined by a `var` and a `;`
+        var_regions = view.find_all(r'var[^;]+;')
+        selected_regions = view.sel()
+
+        # Determine which `var` block each selection is in
         # DEV: If this fails, use esprima to detect var blocks and return locations
-        content_region = sublime.Region(0, view.size())
-        content = view.substr(content_region)
-        print content
+        def find_var_region(selected_region):
+            # By default, no region is found
+            var_region = None
+
+            # For each `var` region
+            for region in var_regions:
+                # If the region contains our selected region, save it and break
+                if region.contains(selected_region):
+                    var_region = region
+                    break
+
+            # Return the found region
+            return var_region
+        selected_var_regions = map(find_var_region, selected_regions)
+
+        print selected_var_regions
 
         # If all selections are in a `var` block
             # Map the selections for a varible
