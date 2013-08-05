0f75a6ad7b59f7df6302400e3270dc9babb07c02
Todd Wolfson <todd@twolfson.com>
2013-08-05 01:27:16 -0700

    Taking notes for self

--- js_var_shortcuts.py
+++ js_var_shortcuts.py
@@ -3,10 +3,26 @@ import sublime_plugin
 class JsVarDeleteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # If the view is not JavaScript, perform the default
-        if self.view.settings().get('syntax') != u'Packages/JavaScript/JavaScript.tmLanguage':
+        view = self.view
+        if view.settings().get('syntax') != u'Packages/JavaScript/JavaScript.tmLanguage':
             return self.run_default()
 
-        print 'hi'
+        # Determine which selections are in a `var` block
+        """
+        // We are only supporting the comma-last format to start
+        // However, they all should be detectable via being between a `var` and a `;`
+        var abc;
+        var abc,
+            def;
+        """
+
+        # If all selections are in a `var` block
+            # Map the selections for a varible
+            # Filter out repeated selections
+            # Delete the selected variable from each block
+
+        # Otherwise, if all selections are not in a `var` block
+            # Run the default command
 
     def run_default(self):
         self.view.run_command("run_macro_file", {
