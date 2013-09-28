84d2dda6505d71b6254997c606e0bfeb07ac0e86
Todd Wolfson <todd@twolfson.com>
2013-09-28 14:45:23 -0700

    Attempting to install plugin tests but failing =(

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='sublime_js_var_shortcuts',
+    version='0.1.0',
+    description='Sublime Text plugin for JS variable shortcuts',
+    keywords=[
+        'sublime text',
+        'plugin',
+        'js',
+        'javascript',
+        'var',
+        'variable',
+        'shortcut'
+    ],
+    author='Todd Wolfson',
+    author_email='todd@twolfson.com',
+    url='https://github.com/twolfson/sublime-js-var-shortcuts',
+    packages=[],
+    license='MIT',
+    install_requires=[],
+    classifiers=[]
+)
